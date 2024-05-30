344f01f6a43e3b954a9ac8739b1e366609f243b7
Dmitrii <stakheev12@mail.ru>
2024-05-30 10:08:43 +0500

    Update 30.05 HotFix footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import {Container} from "@mui/material";
+import {useAppSelector} from "../../redux/hooks";
 
 
 const Footer = () => {
-
+    const theme = useAppSelector((state) => state.user.theme);
 
     return (
-        <Container className={styles["footer"]}>
+        <Container className={styles["footer"] `${theme ? styles["dark"] : styles["light"]}`}>
             © 2024 Все права защищены. Дипломная работа Стахеева Дмитрия. Сделано с любовью и старанием.
         </Container>
     )
